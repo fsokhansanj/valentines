ca6ef41b6ced8d928b32826608d98b168c78b99f
Farz Sokhansanj <fsokhansanj@gmail.com>
2016-01-27 21:43:25 -0800

    Update string_manipulator.rb

--- app/models/string_manipulator.rb
+++ app/models/string_manipulator.rb
@@ -8,8 +8,8 @@ class StringManipulator
 		text_to_array
 		@array_of_answers = Array.new
 		@correct_answers = {
-			'figuero bistro' => 'Good job! Second question. Where did we go for our first trip?',
-			'Dana point' => 'Correct again. Where did DS take you for your birthdya?.'
+			'baltimore' => 'Good job! Second question. On what holiday was our first date?',
+			'valentines' => 'Correct again, but that was easy.'
 		}
 	end
 
